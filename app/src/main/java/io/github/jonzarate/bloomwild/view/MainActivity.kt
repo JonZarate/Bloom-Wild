40f93d7d995ea409b5a57d2e473dcc4717659834
Jon Zarate <jon.zarate@hotmail.com>
2021-03-01 18:55:13 +0000

    Create project

---
+++ app/src/main/java/io/github/jonzarate/bloomwild/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.jonzarate.bloomwild.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.jonzarate.bloomwild.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
